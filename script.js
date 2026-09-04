f2f381d98a1f90cd2b0894bc6e9a870ea0bf5b56
c20246109ella-del <c20246109.ella@csav.edu.ph>
2026-09-04 15:37:04 +0800

    functions

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ console.log(`Age: ${age}`);
 console.log(`Number: ${number}`);
 console.log(`Address: ${address}`);
 console.log(67);
+
+function greet(greetings, names = ["Carl", "bal", "tol", "bai", "perd"]) {
+    if (Array.isArray(names)) {
+        names = names.join(",")
+    }
+    return `${greetings} sa enyu ${names}`;
+}
+console.log(greet("Mayung aga", ["Carl", "bal", "tol", "bai", "perd"]));
+
