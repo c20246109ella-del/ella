430c0e4f20dbb917cdf9ce58c4419abb40c4ab94
c20246109ella-del <c20246109.ella@csav.edu.ph>
2026-09-04 14:37:11 +0800

    variable (let and const)

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-console.log("Hello, World!");
+console.log("Hello world!");
 console.log("From script.js file");
+
+const myName = "Carl";
+const address = "canetown";
+let number = "09606534539";
+let age = 21;
+
+console.log(`Name: ${myName}`)
+console.log(`Age: ${age}`);
+console.log(`Number: ${number}`);
+console.log(`Address: ${address}`);
+console.log(67);
